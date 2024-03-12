d965f531986cb7f439d7e2e908e2e01ccee38423
Kaue Andrade <kapadilha@minha.fag.edu.br>
2024-03-12 15:18:43 -0300

    ajustes header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 
 const StyledHeader = styled.header`
+  background-color: transparent;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
