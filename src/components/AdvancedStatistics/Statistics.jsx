bd9b3ddbbdfef4e64f5ff8b380a5c01f605ebea6
Kaue Andrade <kapadilha@minha.fag.edu.br>
2024-03-12 13:12:23 -0300

    ajustes no texto

--- src/components/AdvancedStatistics/Statistics.jsx
+++ src/components/AdvancedStatistics/Statistics.jsx
@@ -71,21 +71,21 @@ const Statistics = () => {
             <Card
               icon={iconBrand}
               title="Brand Recognition"
-              description="lorem ipsum dolor sit amet consectetur and consectetur adipiscing elit esse cillum dolore magna"
+              description="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content."
             />
           </div>
           <div className="card">
             <Card
               icon={iconDetailed}
               title="Detailed Records"
-              description="lorem ipsum dolor sit amet consectetur and consectetur adipiscing elit esse cillum dolore magna"
+              description="Gain insights into who clicking your links. Knowing when and where people engage with your content helps inform better decisions"
             />
           </div>
           <div className="card">
             <Card
               icon={iconFully}
               title="Fully Customizable"
-              description="lorem ipsum dolor sit amet consectetur and consectetur adipiscing elit esse cillum dolore magna"
+              description="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
             />
           </div>
         </div>
